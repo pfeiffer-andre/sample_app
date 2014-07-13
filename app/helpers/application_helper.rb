c213dbc75973ede6037b13f6d43d548fc08baf26
Jorge Bastos <jorge.bastos@grad.ufsc.br>
2014-07-13 00:52:37 -0300

    quinto commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # titulo padrao
+  def full_title(page_title)
+    #base_title = "Trabalho final"
+	base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
